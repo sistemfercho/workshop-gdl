e2522e0874c99d79a3827267243c013d977c8ae7
The Geek Inside <thegeek@thegeekinside.net>
2013-05-27 20:14:39 -0500

    First release

---
+++ workshop/communication-adapter/src/main/java/com/recluit/workshop/communication/adapter/CommunicationAdapterLauncher.java
@@ -0,0 +1,13 @@
+package com.recluit.workshop.communication.adapter;
+
+/**
+ * Hello world!
+ *
+ */
+public class CommunicationAdapterLauncher
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

3ba86c9b8f69d86d8f348efb13887b7ef33c4c0c
The Geek Inside <thegeek@thegeekinside.net>
2013-06-03 08:45:25 -0500

    Improve the Credit Bureau module.

--- workshop/credit-bureau/src/c/cb.c
+++ workshop/credit-bureau/src/c/cb.c
@@ -1,5 +1,118 @@
-#include <stdio.h>
+/**
+ * Este aplicacion representa a una entidad crediticia la cual
+ * concentra todos los creditos otorgados a los clientes de
+ * diferentes entidades.
+ */
 
-int main() {
-    printf("Hello World");
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 3550 /* El puerto que será abierto */
+#define BACKLOG 2 /* El número de conexiones permitidas */
+
+void doprocessing (int sock)
+{
+    int n;
+    char buffer[256];
+
+    bzero(buffer,256);
+
+    n = read(sock,buffer,255);
+    if (n < 0)
+    {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+    printf("Here is the message: %s\n",buffer);
+    n = write(sock,"I got your message",18);
+    if (n < 0)
+    {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
 }
+
+int main()
+{
+
+   int fd, fd2; /* los descriptores de archivos */
+
+   /* para la información de la dirección del servidor */
+   struct sockaddr_in server;
+
+   /* para la información de la dirección del cliente */
+   struct sockaddr_in client;
+
+   unsigned int sin_size;
+
+   pid_t pid;
+
+   /* A continuación la llamada a socket() */
+   if ((fd=socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
+      printf("error en socket()\n");
+      exit(-1);
+   }
+
+   server.sin_family = AF_INET;
+
+   server.sin_port = htons(PORT);
+
+   server.sin_addr.s_addr = INADDR_ANY;
+   /* INADDR_ANY coloca nuestra dirección IP automáticamente */
+
+   //bzero(&(server.sin_zero),8);
+   memset(&(server.sin_zero), '0', 8);
+   /* escribimos ceros en el reto de la estructura */
+
+
+   /* A continuación la llamada a bind() */
+   if(bind(fd,(struct sockaddr*)&server, sizeof(struct sockaddr))==-1) {
+      printf("error en bind() \n");
+      exit(-1);
+   }
+
+   if(listen(fd,BACKLOG) == -1) {  /* llamada a listen() */
+      printf("error en listen()\n");
+      exit(-1);
+   }
+
+   while(1) {
+      sin_size=sizeof(struct sockaddr_in);
+      /* A continuación la llamada a accept() */
+      if ((fd2 = accept(fd,(struct sockaddr *)&client, &sin_size))==-1) {
+         printf("error en accept()\n");
+         exit(-1);
+      }
+
+      printf("Se obtuvo una conexión desde %s\n", inet_ntoa(client.sin_addr) );
+      /* que mostrará la IP del cliente */
+
+      send(fd2,"Bienvenido a mi servidor.\n",22,0);
+      /* que enviará el mensaje de bienvenida al cliente */
+
+      /* Create child process */
+      pid = fork();
+      if (pid < 0) {
+          perror("ERROR on fork");
+           exit(-1);
+      }
+      if (pid == 0)
+      {
+          /* This is the client process */
+          close(fd);
+          doprocessing(fd2);
+          exit(0);
+      }
+      else
+      {
+          close(fd2);
+      }
+   } /* end while */
+}
+
